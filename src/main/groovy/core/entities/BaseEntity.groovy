e9817a34d76ed8cc3cd7ec30d6d9f7e54f870e5b
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-10 13:21:57 -0300

    feat: cria a entidade base

---
+++ src/main/groovy/core/entities/BaseEntity.groovy
@@ -0,0 +1,17 @@
+package core.entities
+
+import java.time.LocalDateTime
+
+/**
+ * <p>Classe abstrata que representa uma entidade base.</p>
+ * <p>Essa classe contém os atributos comuns a todas as entidades.</p>
+ */
+abstract class BaseEntity {
+    LocalDateTime dateCreated
+    LocalDateTime lastUpdated
+    Boolean deleted = false
+
+    static mapping = {
+        tablePerHierarchy false
+    }
+}
