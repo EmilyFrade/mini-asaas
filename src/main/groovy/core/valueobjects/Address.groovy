dc9e14fd4a6b0c73a388454958276b8b9d60cf3d
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-13 14:05:13 -0300

    feat: torna `nullable` a prop `number` de um `Address`
    Existem endereços sem números e, nesse caso, esse campo não deve ser obrigatório.

--- src/main/groovy/core/valueobjects/Address.groovy
+++ src/main/groovy/core/valueobjects/Address.groovy
@@ -5,11 +5,12 @@ package core.valueobjects
  * <p>Todos os atributos são obrigatórios, exceto o complemento.</p>
  * <p>O {@code zipCode} deve ser uma string de 8 caracteres numéricos, sem formatação.</p>
  * <p>O {@code ufState} deve ser uma sigla de 2 caracteres alfabéticos e maiúsculos.</p>
+ * <p>O {@code number} deve ser um número inteiro positivo ou nulo (caso o endereço não possua número).</p>
  */
 class Address {
 
     String street
-    int number
+    Integer number
     String complement
     String neighborhood
     String city
@@ -18,12 +19,12 @@ class Address {
 
     static constraints = {
         street blank: false, nullable: false
-        number blank: false, nullable: false
+        number blank: false, nullable: true, min: 1
         complement nullable: true
         neighborhood blank: false, nullable: false
         city blank: false, nullable: false
         ufState blank: false, nullable: false, matches: /[A-Z]{2}/
         zipCode blank: false, nullable: false, matches: /\d{8}/
     }
- 
+
 }
