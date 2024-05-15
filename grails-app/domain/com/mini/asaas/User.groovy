ede49b8b61bf4304b6fc68d2927edc023f783555
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-15 13:31:21 -0300

    feat: altera a propriedade username para email e adiciona mais configs ao Spring Security.

--- grails-app/domain/com/mini/asaas/User.groovy
+++ grails-app/domain/com/mini/asaas/User.groovy
@@ -1,22 +1,23 @@
 package com.mini.asaas
 
+import core.entities.BaseEntity
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 import grails.compiler.GrailsCompileStatic
 
 @GrailsCompileStatic
-@EqualsAndHashCode(includes='username')
-@ToString(includes='username', includeNames=true, includePackage=false)
-class User implements Serializable {
+@EqualsAndHashCode(includes='email')
+@ToString(includes='email', includeNames=true, includePackage=false)
+class User extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1
 
-    String username
+    String email
     String password
     boolean enabled = true
-    boolean accountExpired
-    boolean accountLocked
-    boolean passwordExpired
+    boolean accountExpired = false
+    boolean accountLocked = false
+    boolean passwordExpired = false
 
     Set<Role> getAuthorities() {
         (UserRole.findAllByUser(this) as List<UserRole>)*.role as Set<Role>
@@ -24,10 +25,12 @@ class User implements Serializable {
 
     static constraints = {
         password nullable: false, blank: false, password: true
-        username nullable: false, blank: false, unique: true
+        email nullable: false, blank: false, unique: true, email: true
     }
 
     static mapping = {
+        table '`user`'
 	    password column: '`password`'
+        email column: '`email`'
     }
 }
