6ff314496dc14650a2b1add3c53133c3369c7cbb
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-15 14:57:34 -0300

    feat: adiciona roles e user admin ao carregar a aplicação.
    Os nomes das roles e as informações do usuário admin devem constar no arquivo .env da aplicação, a fim de evitar vazamento de dados sensíveis.
    
    Essa configuração pode ser removida posteriormente, caso isso seja considerado uma falha de segurança em produção. Porém, para o atual contexto de textes e aprendizado, a criação automática de um usuário admin e roles básicas fazem sentido.

--- grails-app/init/com/mini/asaas/BootStrap.groovy
+++ grails-app/init/com/mini/asaas/BootStrap.groovy
@@ -1,11 +1,47 @@
 package com.mini.asaas
 
+import grails.core.GrailsApplication
+import grails.gorm.transactions.Transactional
+
 class BootStrap {
 
+    GrailsApplication grailsApplication
+
     def init = { servletContext ->
+        createBasicRoles()
+        createBasicUsers()
     }
 
     def destroy = {
+    }
+
+    @Transactional
+    private void createBasicRoles() {
+        String adminRole = grailsApplication.config.getProperty("security.basic.users.admin.role")
+        String userRole = grailsApplication.config.getProperty("security.basic.users.user.role")
+
+        if (!Role.findByAuthority(adminRole)) {
+            new Role(authority: adminRole).save(failOnError: true)
+        }
+
+        if (!Role.findByAuthority(userRole)) {
+            new Role(authority: userRole).save(failOnError: true)
+        }
+    }
+
+    @Transactional
+    private void createBasicUsers() {
+        String adminEmail = grailsApplication.config.getProperty("security.basic.users.admin.email")
+
+        if (!User.findByEmail(adminEmail)) {
+            String adminRole = grailsApplication.config.getProperty("security.basic.users.admin.role")
+            String userRole = grailsApplication.config.getProperty("security.basic.users.user.role")
+            String adminPassword = grailsApplication.config.getProperty("security.basic.users.admin.password")
+            User admin = new User(email: adminEmail, password: adminPassword)
+            admin.save(failOnError: true)
+            UserRole.create admin, Role.findByAuthority(adminRole)
+            UserRole.create admin, Role.findByAuthority(userRole)
+        }
     }
 
 }
