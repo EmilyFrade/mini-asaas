28a38bf3d618adb3bf2cbb9841e5c11785c3582e
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-16 15:20:20 -0300

    refactor: move as classes para as pastas específicas.

--- src/main/groovy/com/mini/asaas/base/BaseEntity.groovy
+++ src/main/groovy/com/mini/asaas/base/BaseEntity.groovy
@@ -1,14 +1,13 @@
-package core.entities
+package com.mini.asaas.base
 
 import java.time.LocalDateTime
 
-/**
- * <p>Classe abstrata que representa uma entidade base.</p>
- * <p>Essa classe contém os atributos comuns a todas as entidades.</p>
- */
 abstract class BaseEntity {
+
     LocalDateTime dateCreated
+
     LocalDateTime lastUpdated
+
     Boolean deleted = false
 
     static mapping = {
---
+++ src/main/groovy/com/mini/asaas/base/BasePersonData.groovy
@@ -0,0 +1,34 @@
+package com.mini.asaas.base
+
+import com.mini.asaas.shared.enums.PersonType
+import com.mini.asaas.shared.objects.Address
+
+abstract class BasePersonData extends BaseEntity {
+
+    String name
+
+    String email
+
+    String cpfCnpj
+
+    String phoneNumber
+
+    PersonType personType
+
+    Address address
+
+    static constraints = {
+        name blank: false
+        email blank: false, email: true, unique: true
+        cpfCnpj blank: false, size: 11..14
+        phoneNumber blank: false
+        personType blank: false
+        address nullable: false
+    }
+
+    static embedded = ["address"]
+
+    static mapping = {
+        tablePerHierarchy false
+    }
+}
