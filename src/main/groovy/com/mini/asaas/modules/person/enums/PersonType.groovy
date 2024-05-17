a17ea6d19706edd5cbab02792ca891459f5a4e42
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-17 11:36:50 -0300

    refactor: aplica melhores práticas em PersonType.

--- src/main/groovy/com/mini/asaas/modules/person/enums/PersonType.groovy
+++ src/main/groovy/com/mini/asaas/modules/person/enums/PersonType.groovy
@@ -1,4 +1,4 @@
-package com.mini.asaas.shared.enums
+package com.mini.asaas.modules.person.enums
 
 import com.mini.asaas.shared.utils.CpfCnpjUtils
 import grails.compiler.GrailsCompileStatic
@@ -12,10 +12,9 @@ enum PersonType {
     public String getLabel() {
         Locale locale = new Locale("pt", "BR")
         String messageCode = "personType.${this.name()}.label"
-        return Holders
-                .applicationContext
-                .getBean('messageSource')
-                .getMessage(messageCode, null, "", locale)
+        return Holders.applicationContext
+                .getBean("messageSource")
+                .getMessage(messageCode, null, "", Locale.getDefault())
     }
 
     public static PersonType fromString(String value) {
