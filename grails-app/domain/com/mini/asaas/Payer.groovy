14e8e3aaeb8dccd11af676da692fa63bd4aa0a40
Emily Frade <emilyfrade12@gmail.com>
2024-05-17 14:38:53 -0300

    refactor: retira namedQueries desnecessárias

--- grails-app/domain/com/mini/asaas/Payer.groovy
+++ grails-app/domain/com/mini/asaas/Payer.groovy
@@ -2,14 +2,4 @@ package com.mini.asaas
 
 import core.entities.Person
 
-class Payer extends Person {
-    static namedQueries = {
-        if (!Boolean.valueOf(search.includeDeleted)) {
-            eq('deleted', false)
-        }
-
-        if (search.containsKey('id')) {
-            eq('id', search.id)
-        }
-    }
-}
+class Payer extends Person {}
