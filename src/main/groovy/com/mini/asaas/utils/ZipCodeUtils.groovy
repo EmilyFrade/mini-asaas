ed3133bc84e52fd5cddaf2f0a709f5133231b855
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-20 15:29:05 -0300

    refactor: aplica padrão de retornar wrappers ao invés de primitivos

--- src/main/groovy/com/mini/asaas/utils/ZipCodeUtils.groovy
+++ src/main/groovy/com/mini/asaas/utils/ZipCodeUtils.groovy
@@ -5,7 +5,7 @@ import grails.compiler.GrailsCompileStatic
 @GrailsCompileStatic
 class ZipCodeUtils {
 
-    public static Boolean isValid(String zipCode, boolean formatted) {
+    public static Boolean isValid(String zipCode, Boolean formatted) {
         if (zipCode == null) return false
         String regex = formatted ? /\d{5}-\d{3}/ : /\d{8}/
         return zipCode.matches(regex)
