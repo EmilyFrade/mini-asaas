9557f938f6bf7965b3bc4d96352913fd1a4edf4f
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-20 13:38:26 -0300

    test: adiciona testes para PersonType

---
+++ src/test/groovy/com/mini/asaas/enums/PersonTypeTest.groovy
@@ -0,0 +1,60 @@
+package com.mini.asaas.enums
+
+import spock.lang.Specification
+
+class PersonTypeTest extends Specification {
+
+    void "it should check if PersonType is natural"() {
+        expect:
+            PersonType.LEGAL.isNatural() == false
+            PersonType.NATURAL.isNatural() == true
+    }
+
+    void "it should check if PersonType is legal"() {
+        expect:
+            PersonType.LEGAL.isLegal() == true
+            PersonType.NATURAL.isLegal() == false
+    }
+
+    void "it should return correct PersonType according to string values"() {
+        expect:
+            PersonType.fromString(input) == expected
+
+        where:
+            input           | expected
+            "NATURAL"       | PersonType.NATURAL
+            "natural"       | PersonType.NATURAL
+            "LEGAL"         | PersonType.LEGAL
+            "legal"         | PersonType.LEGAL
+    }
+
+    void "it should throw an exception when trying to get the PersonType with a invalid String value"() {
+        when:
+            PersonType.fromString("INVALID")
+
+        then:
+            thrown(IllegalArgumentException)
+    }
+
+    void "it should return correct PersonType according to CPF or CNPJ values"() {
+        expect:
+            PersonType.fromCpfCnpj(input) == expected
+
+        where:
+            input                   | expected
+            "123.456.789-01"        | PersonType.NATURAL
+            "12345678901"           | PersonType.NATURAL
+            "12.345.678/9012-34"    | PersonType.LEGAL
+            "12345678901234"        | PersonType.LEGAL
+    }
+
+    void "it should throw an exception when trying to get the PersonType with a value that is neither CPF nor CNPJ"() {
+        when:
+            PersonType.fromCpfCnpj("INVALID")
+        then:
+            thrown(IllegalArgumentException)
+    }
+
+
+
+}
