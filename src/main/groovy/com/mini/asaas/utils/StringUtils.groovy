23b0a75311a24097617e8e6a58bea10cf847933c
Emily Frade <emilyfrade12@gmail.com>
2024-05-23 11:52:12 -0300

    feat: cria métodos para eliminar espaços iniciais e finais de uma string

--- src/main/groovy/com/mini/asaas/utils/StringUtils.groovy
+++ src/main/groovy/com/mini/asaas/utils/StringUtils.groovy
@@ -15,4 +15,22 @@ class StringUtils {
         return value.substring(0, 1).toLowerCase() + value.substring(1)
     }
 
+    public static Map<String, String> normalizeParams(Map params) {
+
+        if (params == null) return [:]
+
+        Map<String, String> normalizeParams = [:]
+
+        for (String key : params.keySet()) {
+            normalizeParams[key] = ensureStringAndTrim(params[key])
+        }
+
+        return normalizeParams
+    }
+
+    public static String ensureStringAndTrim(Object value) {
+        if (!value || !(value instanceof String)) return null
+        String trimmedValue = (value as String).trim()
+        return trimmedValue.isEmpty() ? null : trimmedValue
+    }
 }
