7493519999a62b6b817e8b86e7e7dba2967e746d
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-23 13:35:10 -0300

    refactor: remove métodos desnecessários em CompanyType

--- src/main/groovy/com/mini/asaas/customer/CompanyType.groovy
+++ src/main/groovy/com/mini/asaas/customer/CompanyType.groovy
@@ -9,32 +9,8 @@ enum CompanyType {
     MEI,
     NON_PROFIT_ASSOCIATION
 
-    public Boolean isAssociation() {
-        return this == ASSOCIATION
-    }
-
-    public Boolean isIndividual() {
-        return this == INDIVIDUAL
-    }
-
-    public Boolean isLimited() {
-        return this == LIMITED
-    }
-
-    public Boolean isMei() {
-        return this == MEI
-    }
-
-    public Boolean isNonProfitAssociation() {
-        return this == NON_PROFIT_ASSOCIATION
-    }
-
     public String getLabel() {
         return MessageSourceUtils.getEnumLabel(this)
-    }
-
-    public static List<CompanyType> getAvailableForCustomerToChoose() {
-        return [ASSOCIATION, INDIVIDUAL, LIMITED, MEI]
     }
 
     public static CompanyType parseFromString(String companyType) {
