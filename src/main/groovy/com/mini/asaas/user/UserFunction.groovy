932c35114fa92b8d7860f2c50548c30b97085b49
Ícaro Teles <icaro.teles@asaas.com.br>
2024-05-31 14:24:52 -0300

    feat: add enum para as funções de usuário

---
+++ src/main/groovy/com/mini/asaas/user/UserFunction.groovy
@@ -0,0 +1,18 @@
+package com.mini.asaas.user
+
+import com.mini.asaas.utils.MessageSourceUtils
+import grails.util.Holders
+
+enum UserFunction {
+    ADMIN,
+    USER
+
+    public String getAuthority() {
+        return Holders.grailsApplication.config.getProperty("security.basic.users.${name().toLowerCase()}.role")
+    }
+
+    public String getLabel() {
+        return MessageSourceUtils.getEnumLabel(this)
+    }
+
+}
