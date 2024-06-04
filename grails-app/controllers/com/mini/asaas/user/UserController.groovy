4417e35b46fdf8468840748c2b6d74575042f686
Ícaro Teles <icaro.teles@asaas.com.br>
2024-06-04 11:44:08 -0300

    feat: implementa lógica e tela de salvar um usuário

--- grails-app/controllers/com/mini/asaas/user/UserController.groovy
+++ grails-app/controllers/com/mini/asaas/user/UserController.groovy
@@ -1,21 +1,51 @@
 package com.mini.asaas.user
 
-
+import com.mini.asaas.auth.AuthService
+import com.mini.asaas.enums.AlertType
+import com.mini.asaas.exceptions.BusinessException
+import com.mini.asaas.user.adapters.LoginUserAdapter
+import com.mini.asaas.user.adapters.SaveUserAdapter
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(["ROLE_ADMIN", "ROLE_SELLER"])
 class UserController {
+
+    AuthService authService
 
     UserService userService
 
     static allowedMethods = [show: "GET"]
 
+    @Secured(["ROLE_ADMIN", "ROLE_SELLER"])
     def show() {
         try {
             User user = userService.show()
             render(view: "show", model: [user: user])
         } catch (Exception e) {
             redirect(uri: "/logout")
+        }
+    }
+
+    @Secured("permitAll")
+    def create() {}
+
+    @Secured("permitAll")
+    def save() {
+        try {
+            userService.save(new SaveUserAdapter(params))
+            flash.status = AlertType.SUCCESS.getValue()
+            flash.message = "Usuário cadastrado com sucesso!"
+            authService.login(new LoginUserAdapter(params))
+
+            redirect(action: "show")
+        } catch (BusinessException e) {
+            flash.message = e.message
+            flash.status = AlertType.ERROR.getValue()
+            redirect(action: "create")
+        } catch (Exception e) {
+            e.printStackTrace()
+            flash.message = "Ocorreu um erro durante o cadastro, aguarde um momento e tente novamente."
+            flash.status = AlertType.ERROR.getValue()
+            redirect(action: "create")
         }
     }
 
--- grails-app/services/com/mini/asaas/user/UserService.groovy
+++ grails-app/services/com/mini/asaas/user/UserService.groovy
@@ -1,5 +1,9 @@
 package com.mini.asaas.user
 
+import com.mini.asaas.exceptions.BusinessException
+import com.mini.asaas.user.adapters.SaveUserAdapter
+import com.mini.asaas.utils.DomainErrorUtils
+import com.mini.asaas.validation.BusinessValidation
 import grails.gorm.transactions.Transactional
 import grails.plugin.springsecurity.SpringSecurityService
 
@@ -8,10 +12,47 @@ class UserService {
 
     SpringSecurityService springSecurityService
 
+    UserRoleService userRoleService
+
     public User show() {
         User user = springSecurityService.loadCurrentUser() as User
         if (!user) throw new RuntimeException("Usuário não encontrado")
 
         return user
     }
+
+    public User save(SaveUserAdapter adapter) {
+        User validatedUser = validateBeforeSave(adapter)
+        if (validatedUser.hasErrors()) {
+            throw new BusinessException(DomainErrorUtils.getFirstValidationMessage(validatedUser))
+        }
+
+        User user = buildUser(adapter, new User())
+        user.save(failOnError: true)
+
+        userRoleService.assignAUserRole(adapter.roleAuthority, user)
+
+        return user
+    }
+
+    private User validateBeforeSave(SaveUserAdapter adapter) {
+        User user = new User();
+        UserValidator validator = new UserValidator()
+        BusinessValidation validationResult = validator.validateBeforeSave(adapter)
+        if (!validationResult.isValid()) {
+            DomainErrorUtils.addBusinessRuleErrors(user, validationResult.errors)
+        }
+
+        return user
+    }
+
+    private User buildUser(SaveUserAdapter adapter, User user) {
+        user.name = adapter.name
+        user.email = adapter.email
+        user.password = adapter.password
+        user.markDirty()
+
+        return user
+    }
+
 }
