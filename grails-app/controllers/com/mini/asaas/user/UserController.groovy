3a7c96b4a6d145efcfb3a2db88e9fb18a8c6d9e3
Ícaro Teles <icaro.teles@asaas.com.br>
2024-06-05 13:39:44 -0300

    fix: remove a possibilidade de criação do usuário admin
    Essa funcionalidade deve ser implementada no fluxo de cadastro do Customer, na tarefa de relacionar as duas entidades

--- grails-app/controllers/com/mini/asaas/user/UserController.groovy
+++ grails-app/controllers/com/mini/asaas/user/UserController.groovy
@@ -1,15 +1,8 @@
 package com.mini.asaas.user
 
-import com.mini.asaas.auth.AuthService
-import com.mini.asaas.enums.AlertType
-import com.mini.asaas.exceptions.BusinessException
-import com.mini.asaas.user.adapters.LoginUserAdapter
-import com.mini.asaas.user.adapters.SaveUserAdapter
 import grails.plugin.springsecurity.annotation.Secured
 
 class UserController {
-
-    AuthService authService
 
     UserService userService
 
@@ -22,31 +15,6 @@ class UserController {
             render(view: "show", model: [user: user])
         } catch (Exception e) {
             redirect(uri: "/logout")
-        }
-    }
-
-    @Secured("permitAll")
-    def create() {}
-
-    @Secured("permitAll")
-    def save() {
-        try {
-            userService.save(new SaveUserAdapter(params))
-            flash.status = AlertType.SUCCESS.getValue()
-            flash.message = "Usuário cadastrado com sucesso!"
-            authService.login(new LoginUserAdapter(params))
-
-            redirect(action: "show")
-        } catch (BusinessException businessException) {
-            flash.message = businessException.message
-            flash.status = AlertType.ERROR.getValue()
-
-            redirect(action: "create")
-        } catch (Exception exception) {
-            flash.message = "Ocorreu um erro durante o cadastro, aguarde um momento e tente novamente."
-            flash.status = AlertType.ERROR.getValue()
-
-            redirect(action: "create")
         }
     }
 
