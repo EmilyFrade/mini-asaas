20cc27634314afc1904c3e1ec46b0059625f8850
Ícaro Teles <icaro.teles@asaas.com.br>
2024-06-06 10:18:47 -0300

    feat: adiciona EmailAdapter

---
+++ src/main/groovy/com/mini/asaas/email/EmailAdapter.groovy
@@ -0,0 +1,42 @@
+package com.mini.asaas.email
+
+import com.mini.asaas.utils.DateFormatUtils
+import com.mini.asaas.utils.Utils
+import grails.compiler.GrailsCompileStatic
+
+@GrailsCompileStatic
+class EmailAdapter {
+
+    String from
+
+    String fromName
+
+    String to
+
+    String replyTo
+
+    List<String> ccList = []
+
+    List<String> bccList = []
+
+    String subject
+
+    String body
+
+    Boolean isHTML = false
+
+    Date sentDate
+
+    public EmailAdapter(Map originalParams) {
+        Map<String, String> params = Utils.normalizeParams(originalParams)
+        if (!params) return
+        this.from = params.from
+        this.fromName = params.fromName
+        this.to = params.to
+        this.replyTo = params.replyTo
+        this.subject = params.subject
+        this.body = params.body
+        this.isHTML = params.isHTML?.toBoolean() ?: false
+        this.sentDate = DateFormatUtils.parseDateFromString(params.sentDate)
+    }
+}
