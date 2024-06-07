4d57850bf5f8a78cd129133e764828d804bf0bf0
Ícaro Teles <icaro.teles@asaas.com.br>
2024-06-07 15:59:06 -0300

    feat(notification): add classe NotificationAdapter
    Este adapter é para uso interno, logo não segue o padrão de normalização de parâmetros como os outros. Só é utilizado para transferência de dados entre as partes da aplicação.

---
+++ src/main/groovy/com/mini/asaas/notification/NotificationAdapter.groovy
@@ -0,0 +1,32 @@
+package com.mini.asaas.notification
+
+import com.mini.asaas.user.User
+import grails.compiler.GrailsCompileStatic
+
+@GrailsCompileStatic
+class NotificationAdapter {
+
+    String title
+
+    String message
+
+    NotificationPriority priority
+
+    NotificationEvent event
+
+    NotificationLink link
+
+    Boolean isRead
+
+    User user
+
+    public NotificationAdapter(Map map) {
+        this.title = map.title
+        this.message = map.message
+        this.priority = map.priority as NotificationPriority
+        this.event = map.event as NotificationEvent
+        this.link = map.link as NotificationLink
+        this.user = map.user as User
+        this.isRead = map.isRead ?: false
+    }
+}
