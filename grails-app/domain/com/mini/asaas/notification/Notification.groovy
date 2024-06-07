cf9c5af5d15d2d88b267736cf07c24623278b1f1
Ícaro Teles <icaro.teles@asaas.com.br>
2024-06-07 15:58:03 -0300

    feat(notification): add classe de domínio Notification

---
+++ grails-app/domain/com/mini/asaas/notification/Notification.groovy
@@ -0,0 +1,36 @@
+package com.mini.asaas.notification
+
+import com.mini.asaas.base.BaseEntity
+import com.mini.asaas.customer.Customer
+import com.mini.asaas.user.User
+
+class Notification extends BaseEntity {
+
+    String title
+
+    String message
+
+    NotificationPriority priority
+
+    NotificationEvent event
+
+    NotificationLink link
+
+    Boolean isRead = false
+
+    Customer customer
+
+    User user
+
+    static constraints = {
+        title blank: false
+        message blank: false
+        link nullable: true, validator: { NotificationLink link, Notification notification ->
+            if (link && !link.validate()) {
+                return "default.invalid.validator.message"
+            }
+        }
+    }
+
+    static embedded = ["link"]
+}
