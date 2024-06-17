1e87ecd6628f1c9c0da09cf0258ed3810a47c45b
Ícaro Teles <icaro.teles@asaas.com.br>
2024-06-17 10:02:49 -0300

    refactor(PaymentEventNotificationService): remove responsabilidade de enviar email, deixa apenas com a de criar e salvar no banco

--- grails-app/services/com/mini/asaas/payment/PaymentEventNotificationService.groovy
+++ grails-app/services/com/mini/asaas/payment/PaymentEventNotificationService.groovy
@@ -1,102 +1,74 @@
 package com.mini.asaas.payment
 
 import com.mini.asaas.email.EmailAdapter
-import com.mini.asaas.email.EmailMessage
 import com.mini.asaas.email.EmailMessageService
-import com.mini.asaas.email.SendEmailService
 import com.mini.asaas.user.User
 import grails.gorm.transactions.Transactional
 import grails.gsp.PageRenderer
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.scheduling.annotation.Async
-import org.springframework.scheduling.annotation.AsyncResult
-
-import java.util.concurrent.Future
 
 @Transactional
 class PaymentEventNotificationService {
 
     EmailMessageService emailMessageService
 
-    SendEmailService sendEmailService
-
     @Autowired
     PageRenderer pageRenderer
 
-    @Async
-    public Future<Void> onSave(Payment payment, User user) {
+    public void onSave(Payment payment, User user) {
         String body = pageRenderer.render(
             template: "/payment/templates/email/payment-created",
             model: [payment: payment, user: user, baseUrl: System.getenv("BASE_URL")]
         )
 
-        EmailMessage emailMessage = emailMessageService.save(new EmailAdapter([
+        emailMessageService.save(new EmailAdapter([
             to     : payment.payer.email,
             subject: "Nova Cobrança",
             body   : body,
             isHTML : true
         ]))
-
-        sendEmailService.send(emailMessage)
-
-        return new AsyncResult<>(null)
     }
 
-    @Async
-    public Future<Void> onDelete(Payment payment, User user) {
+    public void onDelete(Payment payment, User user) {
         String body = pageRenderer.render(
             template: "/payment/templates/email/payment-deleted",
             model: [payment: payment, user: user, baseUrl: System.getenv("BASE_URL")]
         )
 
-        EmailMessage emailMessage = emailMessageService.save(new EmailAdapter([
+        emailMessageService.save(new EmailAdapter([
             to     : payment.payer.email,
             subject: "Cobrança Cancelada",
             body   : body,
             isHTML : true
         ]))
-
-        sendEmailService.send(emailMessage)
-
-        return new AsyncResult<>(null)
     }
 
-    @Async
-    public Future<Void> onReceive(Payment payment, User user) {
+    public void onReceive(Payment payment, User user) {
         String body = pageRenderer.render(
             template: "/payment/templates/email/payment-received",
             model: [payment: payment, user: user, baseUrl: System.getenv("BASE_URL")]
         )
 
-        EmailMessage emailMessage = emailMessageService.save(new EmailAdapter([
+        emailMessageService.save(new EmailAdapter([
             to     : payment.payer.email,
             subject: "Pagamento Recebido",
             body   : body,
             isHTML : true
         ]))
-
-        sendEmailService.send(emailMessage)
-
-        return new AsyncResult<>(null)
     }
 
-    @Async
-    public Future<Void> onOverdue(Payment payment) {
+    public void onOverdue(Payment payment) {
         String body = pageRenderer.render(
             template: "/payment/templates/email/payment-overdue",
             model: [payment: payment, baseUrl: System.getenv("BASE_URL")]
         )
 
-        EmailMessage emailMessage = emailMessageService.save(new EmailAdapter([
+        emailMessageService.save(new EmailAdapter([
             to     : payment.payer.email,
             subject: "Cobrança Vencida",
             body   : body,
             isHTML : true
         ]))
-
-        sendEmailService.send(emailMessage)
-
-        return new AsyncResult<>(null)
     }
 
 }
